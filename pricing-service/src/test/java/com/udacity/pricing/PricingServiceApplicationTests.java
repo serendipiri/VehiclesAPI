7efd0e3c470a95c149c9306328cdfa16dc0c92b0
serendipiri <seren.piri@gmail.com>
2021-08-24 21:46:33 +0300

    pricing service test added

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,53 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PricingServiceApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void getPrice() throws Exception {
+
+		ResponseEntity<Price> response =
+				this.restTemplate.getForEntity("http://localhost:" + port + "/prices/1", Price.class);
+
+		assertThat(response.getBody().getPrice(), equalTo(new BigDecimal(50250.25)));
+		assertThat(response.getBody().getPrice(), not(new BigDecimal(50250.26)));
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+
 	}
 
 }
